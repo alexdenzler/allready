f4366050c4cbffa9d3bbf40cfd187663af062519
eabearden97 <ebearden@macalester.edu>
2020-04-04 13:09:41 -0500

    Changed the name of the homepage to "MaybeReady" to practice pushing to GitHub

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class Home extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-        'AllReady',
+        'MaybeReady',
         style: TextStyle(
           fontSize: 30.0,
           fontWeight: FontWeight.bold,
