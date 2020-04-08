cc5ef862d70768f12b823ef2e883d47c98684cc4
eabearden97 <ebearden@macalester.edu>
2020-04-08 14:56:41 -0500

    Made some arbitrary visual changes, you can now click image and debug console will return "click" (eventually this will lead to a new page)

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,10 @@ void main() => runApp(MaterialApp(
   home: Home(),
 ));
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 class Home extends StatelessWidget {
   @override
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -18,21 +20,34 @@ class Home extends StatelessWidget {
           letterSpacing: 2.0,
           color: Colors.red[600],
           fontFamily: 'ShadowsIntoLight',
-        ), // TextStyle
-      ), // Text
+        ),
+      ),
       centerTitle: true,
       backgroundColor: Colors.blueGrey[300],
-    ), // appBar
+    ),
+
+      // Creates a container with Atrium Picture that you can click on
       body: Container(
-        padding: EdgeInsets.fromLTRB(20,0,20,410),
+        padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+        margin: EdgeInsets.all(10.0),
         color: Colors.blueGrey[300],
-        child: FlatButton(
+        child: RaisedButton(
           onPressed: () {
             print('click');
           },
           child: Image.network('https://themacweekly.com/wp-content/uploads/2018/01/Atrium_news_Rait18-1024x613.jpg'),
-        ), // RaisedButton
-      ), // Center
-    ); // Scaffold
+          ),
+      ),
+
+      // Creates a button in the middle of the screen that says "Click Here to Order"
+        // body: Center(
+        //   child: RaisedButton(
+        //     onPressed: () {
+        //       print('Hello World.');
+        //     },
+        //     child: Text('Click Here To Order'),
+        //     ),
+        //     )
+    );
   }
 }
