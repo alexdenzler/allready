b592259477bcee5ff1e74ecfa39ebf6065d4220a
eabearden97 <ebearden@macalester.edu>
2020-04-08 19:01:57 -0500

    Made pages for each ingredient type specified on ItemTypePage, also made list of breadtype on BreadPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
-import 'dart:js';
-
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// runApp
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 void main() => runApp(MaterialApp(
   home: HomePage(),
 ));
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// HomePage
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 class HomePage extends StatelessWidget {
   @override
@@ -41,80 +44,288 @@ class HomePage extends StatelessWidget {
           child: Image.network('https://themacweekly.com/wp-content/uploads/2018/01/Atrium_news_Rait18-1024x613.jpg'),
           ),
       ),
-      // Creates a button in the middle of the screen that says "Click Here to Order"
-        // body: Center(
-        //   child: RaisedButton(
-        //     onPressed: () {
-        //       print('Hello World.');
-        //     },
-        //     child: Text('Click Here To Order'),
-        //     ),
-        //     )
     );
   }
 }
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ItemTypePage
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 class ItemTypePage extends StatelessWidget {
   @override 
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("AllReady"),
+        title: Text(
+        'AllReady',
+        style: TextStyle(
+          fontSize: 30.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.red[600],
+          fontFamily: 'ShadowsIntoLight',
+        ),
       ),
-      body: getListView(),
+      centerTitle: true,
+      backgroundColor: Colors.blueGrey[300],
+    ),
+      body: getItemTypeList(context),
     );
   }
 }
-
-Widget getListView() {
+// Makes list of buttons for ItemTypePage
+  Widget getItemTypeList(BuildContext context) {
   var listView = ListView(
     children: <Widget> [
-
+      // Bread Option
       ListTile(
         title: Text("Bread"),
-        // onTap: () {
-        //   Navigator.push(
-        //     context,
-        //     MaterialPageRoute(builder: (context) => BreadPage())
-        //   );
-        // },
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => BreadPage())
+          );
+        },
       ),
-
+      // Meat Option
       ListTile(
         title: Text("Meat"),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => MeatPage())
+          );
+        },
       ),
       ListTile(
         title: Text("Cheese"),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => CheesePage())
+          );
+        },
       ),
       ListTile(
         title: Text("Toppings"),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => ToppingsPage())
+          );
+        },
       ),
       ListTile(
         title: Text("Sauce"),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => SaucePage())
+          );
+        },
       ),
     ],
   );
   return listView;
 }
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// BreadPage
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   class BreadPage extends StatelessWidget {
     @override 
     Widget build(BuildContext context) {
       return Scaffold(
         appBar: AppBar(
-          title: Text("AllReady"),
+        title: Text(
+        'AllReady',
+        style: TextStyle(
+          fontSize: 30.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.red[600],
+          fontFamily: 'ShadowsIntoLight',
         ),
+      ),
+      centerTitle: true,
+      backgroundColor: Colors.blueGrey[300],
+      ),
+      body: getBreadList(context),
+    );
+    }
+  }
+  // Makes list of buttons for BreadPage
+  Widget getBreadList(BuildContext context) {
+  var listView = ListView(
+    children: <Widget> [
+      // White Bread
+      ListTile(
+        title: Text("White Bread"),
+        onTap: () {},
+      ),
+      // Wheat Bread
+      ListTile(
+        title: Text("Wheat Bread"),
+        onTap: () {},
+      ),
+      // Multigrain Bread
+      ListTile(
+        title: Text("Multigrain Bread"),
+        onTap: () {},
+      ),
+      // White Wrap
+      ListTile(
+        title: Text("White Wrap"),
+        onTap: () {},
+      ),
+      // Whole Wheat Wrap
+      ListTile(
+        title: Text("Whole Wheat Wrap"),
+        onTap: () {},
+      ),
+      // Spinach Wrap
+      ListTile(
+        title: Text("Spinach Wrap"),
+        onTap: () {},
+      ),
+    ],
+  );
+  return listView;
+}
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// MeatPage
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  class MeatPage extends StatelessWidget {
+    @override 
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+        title: Text(
+        'AllReady',
+        style: TextStyle(
+          fontSize: 30.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.red[600],
+          fontFamily: 'ShadowsIntoLight',
+        ),
+      ),
+      centerTitle: true,
+      backgroundColor: Colors.blueGrey[300],
+    ),
         body: Center(
           child: RaisedButton(
             onPressed: () {
               Navigator.pop(context);
             },
-            child: Text("Go Back to Home Screen."),
+            child: Text("You are on the meat page. Press to go back."),
           ),
         ),
       );
     }
   }
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// CheesePage
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  class CheesePage extends StatelessWidget {
+    @override 
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+        title: Text(
+        'AllReady',
+        style: TextStyle(
+          fontSize: 30.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.red[600],
+          fontFamily: 'ShadowsIntoLight',
+        ),
+      ),
+      centerTitle: true,
+      backgroundColor: Colors.blueGrey[300],
+    ),
+        body: Center(
+          child: RaisedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text("You are on the cheese page. Press to go back."),
+          ),
+        ),
+      );
+    }
+  }
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ToppingsPage
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  class ToppingsPage extends StatelessWidget {
+    @override 
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+        title: Text(
+        'AllReady',
+        style: TextStyle(
+          fontSize: 30.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.red[600],
+          fontFamily: 'ShadowsIntoLight',
+        ),
+      ),
+      centerTitle: true,
+      backgroundColor: Colors.blueGrey[300],
+    ),
+        body: Center(
+          child: RaisedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text("You are on the toppings page. Press to go back."),
+          ),
+        ),
+      );
+    }
+  }
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// SaucePage
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  class SaucePage extends StatelessWidget {
+    @override 
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+        title: Text(
+        'AllReady',
+        style: TextStyle(
+          fontSize: 30.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.red[600],
+          fontFamily: 'ShadowsIntoLight',
+        ),
+      ),
+      centerTitle: true,
+      backgroundColor: Colors.blueGrey[300],
+    ),
+        body: Center(
+          child: RaisedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text("You are on the sauce page. Press to go back."),
+          ),
+        ),
+      );
+    }
+  }
+
 
 // Generic Code for a new page
   // class ItemTypePage extends StatelessWidget {
