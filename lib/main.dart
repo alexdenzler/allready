1b2a48cb0c16359d8bc11e5f9287b2c4c2b1c9fa
Cooperj98 <jjensen4@macalester.edu>
2020-04-09 18:04:57 -0400

    did this work?

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'itemtype.dart';
@@ -16,6 +17,13 @@ class HomePage extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
+    final databaseRef = Firestore.instance;
+    void startOrder() async{
+      print("test!!!!");
+      await databaseRef.collection("orderlists").getDocuments().then((QuerySnapshot snapshot) {
+    snapshot.documents.forEach((f) => print('${f.data}}'));});
+      print("test2");
+    }
     return Scaffold(
       backgroundColor: Colors.blueGrey[350],
       appBar: AppBar(
@@ -40,7 +48,8 @@ class HomePage extends StatelessWidget {
           children: <Widget> [
             Image.network('https://themacweekly.com/wp-content/uploads/2018/01/Atrium_news_Rait18-1024x613.jpg'),
             RaisedButton(
-              onPressed: () {
+              onPressed:() { 
+              startOrder();
                 Navigator.push(
                   context,
                 MaterialPageRoute(builder: (context) => ItemTypePage())
